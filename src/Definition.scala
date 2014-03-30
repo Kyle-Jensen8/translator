5dd85ba4a07b63b7008b8ff7b9012da5d710ed7c
babyhands <kylej072@gmail.com>
2014-03-30 16:08:54 -0700

    First commit of source files!

---
+++ src/Definition.scala
@@ -0,0 +1,39 @@
+/*
+ *The Definition class has a field for an Englisbjecth
+ * word and the Spanish counterpart.    
+ */
+class Definition {
+
+	//Fields to hold the English word 
+	//and the Spanish word 
+	var englishWord: String = _ 
+	var spanishWord: String = _
+
+	/*
+	 * Main Method
+	 */
+	def main(args: Array[String]) {
+		println("Hello, world!")
+}
+
+
+/*   		 
+ * The EnglishDB class has an array of English words
+ * hardcoded in. Think of it as an English Database
+ */
+class EnglishDB {
+	  
+	var eDB : Array[String] =  _
+
+}
+
+/*
+ * The SpanishDB class has an array of Spanish words
+ * hardcoded in. Think of it as an Spanish Database
+ */
+class SpanishDB{
+  
+	var sDB : Array[String] = _
+  
+	}
+}
---
+++ src/Tester.scala
@@ -0,0 +1,9 @@
+object Tester {
+
+	def main(args: Array[String]): Unit = {
+			val a = new Definition
+					a.englishWord = "House"
+					a.spanishWord = "Casa"
+					println(a.englishWord + a.spanishWord) 
+	}
+}
