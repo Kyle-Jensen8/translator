5079ee5960aa14fbb737db60ae7498fef4b0764b
markjgap <markjgap@gmail.com>
2014-03-31 13:16:33 -0700

    added basic Map databases and functions for testing purposes

--- src/Tester.scala
+++ src/Tester.scala
@@ -1,3 +1,5 @@
+import java.util.Random
+
 object Tester {
 
 	def main(args: Array[String]): Unit = {
@@ -5,5 +7,47 @@ object Tester {
 					a.englishWord = "House"
 					a.spanishWord = "Casa"
 					println(a.englishWord + a.spanishWord) 
+					
+	 val db = Map(("hola" -> "hello"),
+              ("adios" -> "goodbye"),
+              ("mucho gusta" -> "nice to meet you"),
+              ("gracias" -> "thank you"),
+              ("de nada" -> "you're welcome"),
+              ("bien" -> "good"))
+              
+    val db1 = Map((1 -> "hola"),
+              (2 -> "adios"),
+              (3 -> "mucho gusta"),
+              (4 -> "gracias"),
+              (5 -> "de nada"),
+              (6 -> "bien"))              
+              
+     val db2 = Map((1 -> "hello"),
+              (2 -> "goodbye"),
+              (3 -> "nice to meet you"),
+              (4 -> "thank you"),
+              (5 -> "you're welcome"),
+              (6 -> "good"))
+    
+    println(db)          
+              
+    // get value at index -> (map(index))
+    println(db("hola"))
+    
+    // checks to see if index is contained in Map -> (map contains index)
+    println(db contains "la casa")
+    
+    // adds to Map -> (map + (index -> val))
+    val newDb = (db + ("buenos dias" -> "good morning"))
+    println(newDb)
+    
+    val rand = new Random(System.currentTimeMillis());
+    val random_index = rand.nextInt(db2.size);
+    val result = db2(random_index);
+    
+    println(result)
+    println(Map() ++ db.map(_.swap))
+    println(db)
+				
 	}
 }
