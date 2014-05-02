1dd488eefbbeafe5d725814d2f5730d340797378
babyhands <kylej072@gmail.com>
2014-05-01 22:32:36 -0700

    createmylist now has creating list and removing list functionality. also migrated to a pane so everything is together for a better user experience

--- src/CreateMyList.scala
+++ src/CreateMyList.scala
@@ -18,28 +18,48 @@ object CreateMyList {
     val userSpanishField = new TextField("")
     
   val tabPane = new TabbedPane{
+      
       val addButton = new Button{
         text = "Add to list!"
       } 
+      val clearButton = new Button{
+        text = "Clear Your List!"
+      }
       
-     def addLabel = new Label {
+       //  val database = new ListView(db.map(_.spanish)) {
+     // } 
+      
+      
+      val addLabel = new Label {
         listenTo(addButton)
         reactions += {
           case ButtonClicked(_) =>
             if (userEnglishField.text == "" || userSpanishField == ""){
               text = "Sorry, you left one or more of the fields blank"
             }
-            else { //make sure this works!!!!
+            else {
               val addSpan = new Spanglish(userEnglishField.text, userSpanishField.text)
-              db:+addSpan
+              db = db:+addSpan
+              //testing to make sure array is being added to
+              db.foreach(println)
               userEnglishField.text = ""
               userSpanishField.text = ""
             }
         }
      }
       
-    
+      val clearLabel = new Label {
+        listenTo(clearButton)
+        reactions += {
+          case ButtonClicked(_) =>
+            var dbReplacement = Array[Spanglish]()
+            db = dbReplacement
+            text = "Your list has been cleared."
+        } 
+      }
+      
       pages += new Page("Creating List", new BorderPanel {
+        //layout += new ScrollPane(database) -> West
         layout += new GridPanel(5,1) {
           border = Swing.EmptyBorder(20, 20, 50, 20)
           contents += new BorderPanel{
@@ -61,6 +81,19 @@ object CreateMyList {
           contents += addLabel
         } -> Center
       })
+    
+     
+      pages += new Page("Clear List", new BorderPanel {
+    	  
+        layout += new GridPanel(4,1){
+          border = Swing.EmptyBorder(20, 20, 50, 20)
+    	    contents += clearButton
+    	    contents += clearLabel
+    	  } -> Center
+     
+     })
+     
+    
     }
     
     val ui: Panel = new BorderPanel {
